79305e4b03c4ccd1e491f1d53e85f6e51e58f190
Katsuya Utada <utada4@gmail.com>
2014-05-03 13:14:38 +0900

    modified:   picasa.sh

--- picasa.sh
+++ picasa.sh
@@ -2,4 +2,12 @@
 
 source /home/utada/.virtualenvs/picasa/bin/activate
 
-./picasa.py --email $1 --password $2 --source /home/utada/Pictures/$3
+if hostname='utada-i7';then
+  user=utada4@gmail.com
+  password=megumegu6
+elif hostname='ThinkPad-T400';then
+  user=utada6@gmail.com
+  password=sizue9109
+fi
+
+./picasa.py --email $user --password $password --source /home/utada/Pictures/$1
